71ed9762f611ba87d17bbed5429e892476e0fdcc
Val Neekman <val@neekware.com>
2018-05-28 17:27:49 -0400

    load svg methods

--- pkgs/mat/src/mat.service.ts
+++ pkgs/mat/src/mat.service.ts
@@ -7,8 +7,40 @@
  */
 
 import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 @Injectable()
 export class MatService {
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer, private mdIconRegistry: MatIconRegistry) {}
+
+  /**
+   * Adds SVG icons within the privided namespace
+   * @param iconList list of icons
+   */
+  loadSvgIconsWithNamespace(iconList) {
+    // export const iconList = [
+    //   {
+    //     names: ['ad', 'AD'], // various alias
+    //     namespace: 'flags',  // namespace
+    //     path: 'assets/images/svg/flags/ad.svg' // path to icon
+    //   },
+    // ]
+
+    iconList.forEach(icon => {
+      const iconSecurePath = this.sanitizer.bypassSecurityTrustResourceUrl(icon.path);
+      icon.names.forEach(name => {
+        this.mdIconRegistry.addSvgIconInNamespace(icon.namespace, name, iconSecurePath);
+      });
+    });
+  }
+
+  /**
+   *
+   * @param iconPath path to icon set (ex: '/assets/fonts/mdi.svg')
+   */
+  loadSvgIconsSet(iconPath: string) {
+    const mdiSecurePath = this.sanitizer.bypassSecurityTrustResourceUrl(iconPath);
+    this.mdIconRegistry.addSvgIconSet(mdiSecurePath);
+  }
 }
